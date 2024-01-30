896a16432ac2f97931d7473761f3a9db5adb6013
Wilson_Dell_Desk <Wilson_Dell_Desk@DESKTOP-7AT2JED>
2024-01-29 19:11:23 -0500

    pusher

--- lib/pusher.ts
+++ lib/pusher.ts
@@ -2,17 +2,10 @@ import PusherServer from "pusher";
 import PusherClient from "pusher-js";
 
 export const pusherServer = new PusherServer({
-<<<<<<< HEAD
   // appId: process.env.PUSHER_APP_ID as string,
   // key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY as string,
   // secret: process.env.PUSHER_SECRET as string,
   //cluster: 'us2',
-=======
-  // appId: process.env.PUSHER_APP_ID!,
-  // key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
-  // secret: process.env.PUSHER_SECRET!,
-  // cluster: 'us2',
->>>>>>> sales
 
   appId : "1740792",
 key:"902514b604c7328f1791",
