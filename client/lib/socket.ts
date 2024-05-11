b74e9a8ad219d14a54cb49f552aeecfe1c3026cd
Wilson_Dell_Desk <Wilson_Dell_Desk@DESKTOP-7AT2JED>
2024-05-10 22:08:11 -0400

    add url to socket

--- client/lib/socket.ts
+++ client/lib/socket.ts
@@ -1,4 +1,5 @@
 import {io} from 'socket.io-client'
-const socket= io('http://localhost:4000')
+// const socket= io('http://localhost:4000')
+const socket= io('https://hperioncrm.com:4000')
 
 export default socket
